888292a2f351a7e196662244f03a80212904f752
ilyaigpetrov <ilyaigpetrov@gmail.com>
2025-10-31 11:28:42 +0500

    Migrate from `::set-output` to `$GITHUB_OUTPUT`
    See <https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/>.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ ipfs pin remote service add origin $ENDPOINT $ACCESS_TOKEN
 CID=$(ipfs add -r -q --cid-version 1 $UPLOAD_PATH | tail -1)
 ipfs pin remote add --service=origin --name=$NAME $CID
 
-echo "::set-output name=cid::$CID"
+echo "cid=$CID" >> $GITHUB_OUTPUT
